1b3e1d0e3ff5aa938ca7b9cf2d4322f96c75bb19
Katie <katiebutler37@gmail.com>
2022-09-08 12:12:42 -0400

    comment functionality added

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -8,6 +8,12 @@ const pizzaController = {
   getAllPizza(req, res) {
     //mongoose find method
     Pizza.find({})
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
@@ -15,23 +21,26 @@ const pizzaController = {
       });
   },
 
-  // get one pizza by id
-  getPizzaById({ params }, res) {
-    //mongoose findOne method
-    Pizza.findOne({ _id: params.id })
-      .then(dbPizzaData => {
-        // If no pizza is found, send 404
-        if (!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
-          return;
-        }
-        res.json(dbPizzaData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-      });
-  },
+ // get one pizza by id
+getPizzaById({ params }, res) {
+  Pizza.findOne({ _id: params.id })
+    .populate({
+      path: 'comments',
+      select: '-__v'
+    })
+    .select('-__v')
+    .then(dbPizzaData => {
+      if (!dbPizzaData) {
+        res.status(404).json({ message: 'No pizza found with this id!' });
+        return;
+      }
+      res.json(dbPizzaData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+},
   // createPizza
 createPizza({ body }, res) {
     //mongoose method create
